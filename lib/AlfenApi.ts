5b017a7b7eabd45284fa92beda023755024f5bf7
Robert Raaijmakers <robertraaijmakers@gmail.com>
2025-02-12 21:31:20 +0100

    First attempt to read extra properties from charepoint, to be able to set them at a later stage

--- lib/AlfenApi.ts
+++ lib/AlfenApi.ts
@@ -5,17 +5,24 @@ import { HttpsPromiseOptions, HttpsPromiseResponse, InfoResponse, PropertyRespon
 import https from 'https';
 
 const energyMeterCapabilitiesMap: { [key: string]: string } = {
-  '2221_16': 'measure_power',
-  // '2501_2': 'onoff',
-  '2221_22': 'meter_power',
-  '2221_A': 'measure_current.l1',
-  '2221_B': 'measure_current.l2',
-  '2221_C': 'measure_current.l3',
+  '2062_0': 'measure_current.stationlimit',
+  '2129_0': 'measure_current.limit',
+  '2201_0': 'measure_temperature',
   '2221_3': 'measure_voltage.l1',
   '2221_4': 'measure_voltage.l2',
   '2221_5': 'measure_voltage.l3',
-  '2201_0': 'measure_temperature',
-  '2129_0': 'measure_current.limit',
+  '2221_10': 'measure_current.l1',
+  '2221_11': 'measure_current.l2',
+  '2221_12': 'measure_current.l3',
+  '2221_16': 'measure_power',
+  '2221_19': 'meter_power.l1',
+  '2221_20': 'meter_power.l2',
+  '2221_21': 'meter_power.l3',
+  '2221_22': 'meter_power',
+  '2501_2': 'onoff',
+  '3280_1': 'chargetype',
+  '3280_2': 'greenshare',
+  '3280_3': 'comfortchargelevel',
 };
 
 export class AlfenApi {
@@ -130,7 +137,7 @@ export class AlfenApi {
 
   async apiGetActualValues() {
     // Define the 'ids' parameter
-    const ids = '2060_0,2056_0,2221_3,2221_4,2221_5,2221_A,2221_B,2221_C,2221_16,2201_0,2501_2,2221_22,2129_0,2126_0';
+    const ids = '2056_0,2060_0,2062_0,2126_0,2129_0,2201_0,2221_3,2221_4,2221_5,2221_10,2221_11,2221_12,2221_16,2221_19,2221_20,2221_21,2221_22,2501_2,3280_1,3280_2,3280_3';
 
     // Define the options for the HTTPS request (no body, just headers)
     const options: HttpsPromiseOptions = {
@@ -180,9 +187,18 @@ export class AlfenApi {
             case '2221_16': // Power (watts)
               value = Math.round(prop.value * 10) / 10; // rounding values, one decimal
               break;
+            case '2221_19': // Energy L1
+            case '2221_20': // Energy L2
+            case '2221_21': // Energy L3
             case '2221_22': // Total energy
               value = Math.round(prop.value / 10) / 100; // rounding values, 2 decimal (but needs to be devided by 1000)
               break;
+            case '3280_1': // Operating mode
+              this.#log('Operating mode:', prop.value);
+              value = 'normal';
+              break;
+            case '3280_3':
+              this.#log('Comfort charge level:', prop.value);
             default:
               value = prop.value;
               break;
@@ -197,6 +213,83 @@ export class AlfenApi {
     } catch (error) {
       this.#log('Request failed:', error);
       throw new Error(`Request failed: ${error}`);
+    }
+  }
+
+  async apiSetGreenSharePercentage(percentage: number) {
+    if (percentage < 0 || percentage > 100) return false;
+
+    // Define the request body
+    const body = JSON.stringify({
+      '3280_2': {
+        id: '3280_2',
+        value: percentage,
+      },
+    });
+
+    await this.#apiSetProperty(body);
+  }
+
+  async apiSetComfortCharging(comfortChargingkWh: number) {
+    if (comfortChargingkWh < 1400 || comfortChargingkWh > 5000) return false;
+
+    // Define the request body
+    const body = JSON.stringify({
+      '3280_3': {
+        id: '3280_3',
+        value: comfortChargingkWh,
+      },
+    });
+
+    await this.#apiSetProperty(body);
+  }
+
+  async apiSetCurrentLimit(currentLimit: number) {
+    if (currentLimit < 1 || currentLimit > 32) return false;
+
+    // Define the request body
+    const body = JSON.stringify({
+      '2129_0': {
+        id: '2129_0',
+        value: currentLimit,
+      },
+    });
+
+    await this.#apiSetProperty(body);
+  }
+
+  async apiRebootEvCharger() {
+    //self._post(cmd=CMD, payload={PARAM_COMMAND: "reboot"})
+    //`HTTP POST https://<HOST_IP>/api/cmd`
+    //```
+    //{"command":"reboot"}
+    //```
+  }
+
+  async #apiSetProperty(body: string) {
+    // Define the options for the HTTPS request
+    const options = {
+      hostname: this.#ip,
+      path: `/${this.#apiUrl}/prop`,
+      method: 'POST',
+      headers: {
+        'Content-Type': this.#apiHeader,
+        'Content-Length': Buffer.byteLength(body).toString(),
+        Connection: 'keep-alive',
+      },
+      agent: this.#agent,
+      rejectUnauthorized: false, // Disable SSL certificate validation if needed
+    };
+
+    try {
+      // Make the HTTPS request using the httpsPromise method
+      const response = await this.#httpsPromise({ ...options, body });
+
+      // Handle the response
+      this.#log('Written property:', response.body);
+    } catch (error) {
+      this.#log('Property write failed:', error);
+      throw new Error(`Property write failed: ${error}`);
     }
   }
 
